f504eaa0cf3f10c475b3ee21176deac76b0c7739
Gabriel Turato <gabrielturato@outlook.com>
2024-12-26 12:42:39 -0300

    Entidade Bloco

---
+++ src/main/java/com/devsuperior/evento/entities/Bloco.java
@@ -0,0 +1,52 @@
+package com.devsuperior.evento.entities;
+
+import jakarta.persistence.*;
+
+import java.time.Instant;
+
+@Entity
+@Table(name = "tb_bloco")
+public class Bloco {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    @Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
+    private Instant inicio;
+    @Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
+    private Instant fim;
+    @ManyToOne
+    @JoinColumn(name = "id_atividade")
+    private Atividade atividade;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Instant getInicio() {
+        return inicio;
+    }
+
+    public void setInicio(Instant inicio) {
+        this.inicio = inicio;
+    }
+
+    public Instant getFim() {
+        return fim;
+    }
+
+    public void setFim(Instant fim) {
+        this.fim = fim;
+    }
+
+    public Atividade getAtividade() {
+        return atividade;
+    }
+
+    public void setAtividade(Atividade atividade) {
+        this.atividade = atividade;
+    }
+}
